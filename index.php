cc2ef42504561d398b6cf7dc8d51273b14b68bc8
Trollboy777 <Emregulec70@gmail.com>
2024-01-11 13:48:34 +0100

    yhh

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Kidzglobe login</title>
-<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">-->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="./css/home.css">
 </head>
 <body>
 <header>
