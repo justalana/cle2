e27706a5246dd8c8b4d5f139c869fe9ea0e9e372
hugob <1078703@hr.nl>
2024-01-11 14:36:52 +0100

    yeah

---
+++ login/login.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="login.css">
+    <title>KidzGlobe</title>
+</head>
+<body>
+
+<nav>
+    <div id="nav">
+        <div id="logo">
+            <img src="./images/Kidz-Globe-logo%20(1).png" alt="" loading="lazy">
+        </div>
+        <div id="link">
+            <div>
+                <a href="">Home</a>
+            </div>
+
+            <div>
+                <a href="">Inschrijven</a>
+            </div>
+
+            <div>
+                <a href="">Over ons</a>
+            </div>
+
+            <div>
+                <a href="">Contact</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<section>
+    <div>
+        <h1>Log in</h1>
+    </div>
+    <div>
+        <h3>Beste ouder/verzorger, log in om te kunnen inplannen.</h3>
+    </div>
+    <form action="">
+        <div>
+            <div>
+                <input type="text" id="email" name="email" placeholder="Email" required>
+            </div>
+
+            <div>
+                <input type="text" id="wachtwoord" name="wachtwoord" placeholder="Wachtwoord" required>
+            </div>
+        </div>
+        <button class="button">Inloggen</button>
+    </form>
+</section>
+
+</body>
+</html>
