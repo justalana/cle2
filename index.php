b5ac2ba72a406331dabf8b650315c78527398181
Trollboy777 <Emregulec70@gmail.com>
2024-01-11 12:59:06 +0100

    yhh

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
 <?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kidzglobe login</title>
+<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <div class="logo">
+        <img src="images/Kidz-Globe-logo%20(1).png" alt="Kidzglobe">
+    </div>
+    <h2>Home</h2>
+    <h2>Reservering Maken</h2>
+    <h2>Over Ons</h2>
+    <h2>Contact</h2>
+</header>
+<main>
+
+    <h1 class="title">
+        Welkom bij KidzGlobe
+    </h1>
+
+        <div class="name-container">
+         <a href="login.php" class="button">Inloggen</a>
+            <a href="#" class="button">Maak account</a>
+
+        </div>
+
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
