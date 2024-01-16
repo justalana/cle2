06a0cd1bb67850a7101a6a9964b8182d95a70457
Trollboy777 <Emregulec70@gmail.com>
2024-01-16 10:59:33 +0100

    connection .php file made (localhost phpmyadmin)

--- connection.php
+++ connection.php
@@ -1,9 +1,21 @@
 <?php
 
+//$host       = "localhost";
+//$username       = "prj_2023_2024_ressys_t12";
+//$password   = "eethaeng";
+//$database   = "prj_2023_2024_ressys_t12";
+// gegevens online database
 $host       = "localhost";
+$username       = "root";
+$password   = "";
 $database   = "prj_2023_2024_ressys_t12";
-$user       = "root";
-$password   = "prj_2023_2024_ressys_t12";
 
-$db = mysqli_connect($host, $user, $password, $database)
+$db = mysqli_connect($host, $username, $password, $database)
 or die("Error: " . mysqli_connect_error());
+// Stap 1: Verbinding leggen met de database
+// Stap 2: Foutafhandeling. Als verbinding niet gelukt is, wordt
+//         "or die" uitgevoerd. Deze stopt de code en toont de
+//         foutmelding op het scherm
+// local databse
+$db = mysqli_connect($host, $username, $password, $database)
+or die('Error: '.mysqli_connect_error());
